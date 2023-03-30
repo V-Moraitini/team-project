1c08d40f3ee656a6b4b95d35ac969de357e9dd5f
TwctlccGM <twctlccgm@gmail.com>
2023-03-30 19:27:08 +0100

    First commit woo

---
+++ src/GUI.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class GUI {
+    private JPanel Panel_Main;
+    private JTextField usernameTextField;
+    private JTextField passwordTextField;
+    private JButton loginButton;
+}
