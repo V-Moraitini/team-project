ed956bb91c69c33fe9f9f8118385c2a2beb22a2f
TwctlccGM <twctlccgm@gmail.com>
2023-04-01 20:55:16 +0100

    Fifth commit - Added more forms and started System Advisor forms. Gonna try get all the forms done before I have to start doing Games Tech CW.

---
+++ src/GUI/GUI_SA_ArchiveBlank.java
@@ -0,0 +1,13 @@
+package GUI;
+
+import javax.swing.*;
+
+public class GUI_SA_ArchiveBlank {
+    private JPanel panel1;
+    private JList list1;
+    private JList list2;
+    private JButton removeBlankButton;
+    private JButton archiveButton;
+    private JButton backButton;
+    private JButton logoutButton;
+}
---
+++ src/GUI/GUI_SystemAdmin.java
@@ -0,0 +1,13 @@
+package GUI;
+
+import javax.swing.*;
+
+public class GUI_SystemAdmin {
+    private JPanel panel1;
+    private JButton accessBlankStockButton;
+    private JButton manageStaffAccountButton;
+    private JButton accessDatabaseButton;
+    private JButton accessCommissionButton;
+    private JButton createCommissionButton;
+    private JButton logoutButton;
+}
