9dcb4e58c8655578a5a8e7c8839b39be492adbb2
TwctlccGM <twctlccgm@gmail.com>
2023-04-01 13:49:46 +0100

    Fourth commit - Added more forms.

---
+++ src/GUI/GUI_OM_AssignBlankStock.java
@@ -0,0 +1,11 @@
+package GUI;
+
+import javax.swing.*;
+
+public class GUI_OM_AssignBlankStock {
+    private JPanel panel1;
+    private JButton reassignBlanksButton;
+    private JButton accessBlankStockButton;
+    private JButton logoutButton;
+    private JButton backButton;
+}
---
+++ src/GUI/GUI_OM_ReassignBlank.java
@@ -0,0 +1,14 @@
+package GUI;
+
+import javax.swing.*;
+
+public class GUI_OM_ReassignBlank {
+    private JPanel panel1;
+    private JComboBox comboBox1;
+    private JTextField textField1;
+    private JButton button1;
+    private JButton logoutButton;
+    private JTextField textField2;
+    private JButton button2;
+    private JButton backButton;
+}
