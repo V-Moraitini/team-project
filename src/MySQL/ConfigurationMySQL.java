654ea151c4528464f683e4d393982fc8cb8dd394
V-Moraitini <91741381+V-Moraitini@users.noreply.github.com>
2023-04-05 00:56:42 +0100

    rework on the mysql config after the tables were changed

--- src/MySQL/ConfigurationMySQL.java
+++ src/MySQL/ConfigurationMySQL.java
@@ -3,66 +3,98 @@ package MySQL;
 import java.sql.*;
 
 public class ConfigurationMySQL {
+    Connection con;
 
-    public static void getUsers() {
+    public ConfigurationMySQL() {
         try {
-            Connection con = DriverManager.getConnection(
-                    "jdbc:mysql://smcse-stuproj00.city.ac.uk:3306/in2018g07","in2018g07_d","6KV8dzpF");
+            this.con = DriverManager.getConnection(
+                    "jdbc:mysql://smcse-stuproj00.city.ac.uk:3306/in2018g07", "in2018g07_d", "6KV8dzpF");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+    }
+
+    public void closeConnection() throws SQLException {
+        con.close();
+    }
+
+    public void getUsers() {
+        try {
             Statement stmt = con.createStatement();
             ResultSet rs = stmt.executeQuery("SELECT * FROM userAccount");
             while( rs.next() )
-                //userID, userAgencyTravelCode, userName, userAddress, userEmail, userPassword, userType, userIsArchived
+                //userID, userAgencyTravelCode, userName, userPassword, userType, userIsArchived
                 System.out.println(rs.getInt(1)+"  "+rs.getInt(2)+"  "+rs.getString(3)+"  "+rs.getString(4)+"  "
-                        +rs.getString(5)+"  "+rs.getString(6)+"  "+rs.getString(7)+"  "+rs.getInt(8)+"\n");
+                        +rs.getString(5)+"  "+rs.getInt(6)+"\n");
                 //when objects have been made, use object constructor to make them?
-            con.close();
+
         } catch (Exception e) { System.out.println(e); }
     }
 
-    public static void getUserById(int id) {
+    public void getUserById(int id) {
         try {
-            Connection con = DriverManager.getConnection(
-                    "jdbc:mysql://smcse-stuproj00.city.ac.uk:3306/in2018g07","in2018g07_d","6KV8dzpF");
             PreparedStatement stmt = con.prepareStatement("SELECT * FROM userAccount WHERE userID = ?");
             stmt.setInt(1, id);
             ResultSet rs = stmt.executeQuery();
             while( rs.next() )
-                //userID, userAgencyTravelCode, userName, userAddress, userEmail, userPassword, userType, userIsArchived
+                //userID, userAgencyTravelCode, userName, userPassword, userType, userIsArchived
                 System.out.println(rs.getInt(1)+"  "+rs.getInt(2)+"  "+rs.getString(3)+"  "+rs.getString(4)+"  "
-                        +rs.getString(5)+"  "+rs.getString(6)+"  "+rs.getString(7)+"  "+rs.getInt(8)+"\n");
+                        +rs.getString(5)+"  "+rs.getInt(6)+"\n");
             //when objects have been made, use object constructor to make them?
-            con.close();
+
         } catch (Exception e) { System.out.println(e); }
     }
 
-    public static void updateUserById(int id, String name, String address, String email, String password, String type) {
+    public void updateUserById(int id, String name, String password, String type) {
         try {
-            Connection con = DriverManager.getConnection(
-                    "jdbc:mysql://smcse-stuproj00.city.ac.uk:3306/in2018g07","in2018g07_d","6KV8dzpF");
             PreparedStatement stmt = con.prepareStatement(
-                    "UPDATE userAccount SET userName=?, userAddress=?, userEmail=?, userPassword=?, userType=? " +
+                    "UPDATE userAccount SET userName=?, userPassword=?, userType=? " +
                     "WHERE userID = ?");
             stmt.setString(1, name);
-            stmt.setString(2, address);
-            stmt.setString(3, email);
-            stmt.setString(4, password);
-            stmt.setString(5, type);
-            stmt.setInt(6, id);
-            ResultSet rs = stmt.executeQuery();
-            while( rs.next() )
-                //userID, userAgencyTravelCode, userName, userAddress, userEmail, userPassword, userType, userIsArchived
-                //System.out.println(rs.getInt(1)+"  "+rs.getInt(2)+"  "+rs.getString(3)+"  "+rs.getString(4)+"  "
-                //        +rs.getString(5)+"  "+rs.getString(6)+"  "+rs.getString(7)+"  "+rs.getInt(8)+"/n");
-            //when objects have been made, use object constructor to make them?
+            stmt.setString(2, password);
+            stmt.setString(3, type);
+            stmt.setInt(4, id);
+            con.setAutoCommit(false);
+            stmt.executeUpdate();
             con.commit();
         } catch (Exception e) {
             System.out.println(e);
         }
     }
 
+    public static void archiveUser(int id) {
+        try {
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://smcse-stuproj00.city.ac.uk:3306/in2018g07","in2018g07_d","6KV8dzpF");
+            PreparedStatement stmt = con.prepareStatement(
+                    "UPDATE userAccount SET userName=?, userPassword=?, userType=? " +
+                            "WHERE userID = ?");
 
-    public static void main(String args[]){
-        getUsers();
-        getUserById(1);
+            stmt.setInt(4, id);
+            ResultSet rs = stmt.executeQuery();
+            while( rs.next() )
+                //userID, userAgencyTravelCode, userName, userAddress, userEmail, userPassword, userType, userIsArchived
+                //System.out.println(rs.getInt(1)+"  "+rs.getInt(2)+"  "+rs.getString(3)+"  "+rs.getString(4)+"  "
+                //        +rs.getString(5)+"  "+rs.getString(6)+"  "+rs.getString(7)+"  "+rs.getInt(8)+"/n");
+                //when objects have been made, use object constructor to make them?
+                con.commit();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void main(String args[]) throws SQLException {
+        ConfigurationMySQL a = new ConfigurationMySQL();
+        try {
+            a.getUsers();
+            a.getUserById(1);
+            a.updateUserById(1, "Max Jenkins", "tod123", "Office Manager");
+        } catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            a.closeConnection();
+        }
+
     }
 }
