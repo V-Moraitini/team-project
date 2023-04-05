7c3c9664be3087bbb704378b6ef57e92fa16bb25
V-Moraitini <91741381+V-Moraitini@users.noreply.github.com>
2023-04-05 16:21:53 +0100

    archive user is a thing now

--- src/MySQL/ConfigurationMySQL.java
+++ src/MySQL/ConfigurationMySQL.java
@@ -58,27 +58,40 @@ public class ConfigurationMySQL {
             con.setAutoCommit(false);
             stmt.executeUpdate();
             con.commit();
+            con.setAutoCommit(true);
         } catch (Exception e) {
             System.out.println(e);
         }
     }
 
-    public static void archiveUser(int id) {
+    public void archiveUser(int id) {
         try {
-            Connection con = DriverManager.getConnection(
-                    "jdbc:mysql://smcse-stuproj00.city.ac.uk:3306/in2018g07","in2018g07_d","6KV8dzpF");
-            PreparedStatement stmt = con.prepareStatement(
-                    "UPDATE userAccount SET userName=?, userPassword=?, userType=? " +
-                            "WHERE userID = ?");
+            PreparedStatement isArchived = con.prepareStatement("SELECT userIsArchived FROM userAccount WHERE userID = ?");
+            isArchived.setInt(1, id);
+            ResultSet rsArchived = isArchived.executeQuery();
+            //System.out.println(rsArchived.next());
+            while (rsArchived.next()) {
+                if (rsArchived.getInt(1) == 0) {
+                    PreparedStatement stmt = con.prepareStatement(
+                            "UPDATE userAccount SET userIsArchived = 1 WHERE userID = ?");
+                    stmt.setInt(1, id);
+                    con.setAutoCommit(false);
+                    stmt.executeUpdate();
+                    con.commit();
+                    con.setAutoCommit(true);
+                    System.out.println("User is now archived.");
+                } else {
+                    PreparedStatement stmt = con.prepareStatement(
+                            "UPDATE userAccount SET userIsArchived = 0 WHERE userID = ?");
+                    stmt.setInt(1, id);
+                    con.setAutoCommit(false);
+                    stmt.executeUpdate();
+                    con.commit();
+                    con.setAutoCommit(true);
+                    System.out.println("User is no longer archived.");
+                }
+            }
 
-            stmt.setInt(4, id);
-            ResultSet rs = stmt.executeQuery();
-            while( rs.next() )
-                //userID, userAgencyTravelCode, userName, userAddress, userEmail, userPassword, userType, userIsArchived
-                //System.out.println(rs.getInt(1)+"  "+rs.getInt(2)+"  "+rs.getString(3)+"  "+rs.getString(4)+"  "
-                //        +rs.getString(5)+"  "+rs.getString(6)+"  "+rs.getString(7)+"  "+rs.getInt(8)+"/n");
-                //when objects have been made, use object constructor to make them?
-                con.commit();
         } catch (Exception e) {
             System.out.println(e);
         }
@@ -87,9 +100,10 @@ public class ConfigurationMySQL {
     public static void main(String args[]) throws SQLException {
         ConfigurationMySQL a = new ConfigurationMySQL();
         try {
-            a.getUsers();
+            //a.getUsers();
             a.getUserById(1);
-            a.updateUserById(1, "Max Jenkins", "tod123", "Office Manager");
+            //a.updateUserById(1, "Max Jenkins", "tod123", "Office Manager");
+            a.archiveUser(1);
         } catch (Exception e) {
             System.out.println(e);
         } finally {
