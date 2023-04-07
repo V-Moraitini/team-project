37591bfcc6134668e1b147a6704ed50f51f953ad
V-Moraitini <91741381+V-Moraitini@users.noreply.github.com>
2023-04-07 18:58:56 +0100

    user queries are done onwards to customers!

--- src/MySQL/ConfigurationMySQL.java
+++ src/MySQL/ConfigurationMySQL.java
@@ -9,8 +9,8 @@ public class ConfigurationMySQL {
         try {
             this.con = DriverManager.getConnection(
                     "jdbc:mysql://smcse-stuproj00.city.ac.uk:3306/in2018g07", "in2018g07_d", "6KV8dzpF");
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
 
     }
@@ -19,7 +19,42 @@ public class ConfigurationMySQL {
         con.close();
     }
 
-    public void getUsers() {
+    /*-------------------------USER QUERIES START-------------------------*/
+    //Create user (with ID)
+    public void createUser(int id, String name, String password, String type) {
+        try {
+            PreparedStatement stmt = con.prepareStatement(
+                    "INSERT INTO userAccount VALUES (?, 1, ?, ?, ?, 0)");
+            //Statement.RETURN_GENERATED_KEYS for auto generated keys
+            stmt.setInt(1, id);
+            stmt.setString(2, name);
+            stmt.setString(3, password);
+            stmt.setString(4, type);
+            con.setAutoCommit(false);
+            stmt.executeUpdate();
+            con.commit();
+            con.setAutoCommit(true);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    //Getting active non-archived users only
+    public void getActiveUsers() {
+        try {
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM userAccount WHERE userIsArchived = 0");
+            while( rs.next() )
+                //userID, userAgencyTravelCode, userName, userPassword, userType, userIsArchived
+                System.out.println(rs.getInt(1)+"  "+rs.getInt(2)+"  "+rs.getString(3)+"  "+rs.getString(4)+"  "
+                        +rs.getString(5)+"  "+rs.getInt(6)+"\n");
+                //when objects have been made, use object constructor to make them?
+
+        } catch (Exception e) { System.out.println(e); }
+    }
+
+    //Getting users regardless of whether they are archived or not
+    public void getAllUsers() {
         try {
             Statement stmt = con.createStatement();
             ResultSet rs = stmt.executeQuery("SELECT * FROM userAccount");
@@ -66,7 +101,8 @@ public class ConfigurationMySQL {
 
     public void archiveUser(int id) {
         try {
-            PreparedStatement isArchived = con.prepareStatement("SELECT userIsArchived FROM userAccount WHERE userID = ?");
+            PreparedStatement isArchived = con.prepareStatement(
+                    "SELECT userIsArchived FROM userAccount WHERE userID = ?");
             isArchived.setInt(1, id);
             ResultSet rsArchived = isArchived.executeQuery();
             //System.out.println(rsArchived.next());
@@ -96,14 +132,66 @@ public class ConfigurationMySQL {
             System.out.println(e);
         }
     }
+    /*-------------------------USER QUERIES END-------------------------*/
+
+    /*-------------------------CUSTOMER QUERIES START-------------------------*/
+    public void createCustomer(String name, String alias, String email, int phone, Boolean isValued) {
+        try {
+            PreparedStatement stmt = con.prepareStatement(
+                    "INSERT INTO customer " +
+                            "(customerName, customerAlias, customerEmail, customerPhone, customerIsValued, customerFixedDiscountPercentage)" +
+                            "VALUES (?, ?, ?, ?, ?, NULL)");
+            //Statement.RETURN_GENERATED_KEYS for auto generated keys
+            stmt.setString(1, name);
+            stmt.setString(2, alias);
+            stmt.setString(3, email);
+            stmt.setInt(4, phone);
+            stmt.setBoolean(5, isValued);
+            con.setAutoCommit(false);
+            stmt.executeUpdate();
+            /*int affectedRows = stmt.executeUpdate();
+            // check the affected rows
+            if (affectedRows > 0) {
+                // get the ID back
+                try (ResultSet rs = stmt.getGeneratedKeys()) {
+                    if (rs.next()) {
+                        id = rs.getInt(1);
+                    }
+                } catch (SQLException e) {
+                    System.out.println(e.getMessage());
+                }
+            }*/
+            con.commit();
+            con.setAutoCommit(true);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void getCustomers() {
+        try {
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM customer");
+            while( rs.next() )
+                //customerID, customerName, customerAlias, customerEmail, customerPhone, customerIsValued, customerFixedDiscountPercentage
+                System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3)+"  "+rs.getString(4)+"  "
+                        +rs.getInt(5)+"  "+rs.getBoolean(6)+"  "+rs.getDouble(7)+"\n");
+            //when objects have been made, use object constructor to make them?
+
+        } catch (Exception e) { System.out.println(e); }
+    }
+
+    /*-------------------------CUSTOMER QUERIES END-------------------------*/
 
     public static void main(String args[]) throws SQLException {
         ConfigurationMySQL a = new ConfigurationMySQL();
         try {
             //a.getUsers();
-            a.getUserById(1);
-            //a.updateUserById(1, "Max Jenkins", "tod123", "Office Manager");
-            a.archiveUser(1);
+            //a.getUserById(1);
+            //a.updateUserById(1, "Todd Jenkins", "tod123", "Office Manager");
+            //a.archiveUser(1);
+            //a.createCustomer("Chris Smart", "Chris", "chris.smart@email.com", 7888111, true);
+            a.getCustomers();
         } catch (Exception e) {
             System.out.println(e);
         } finally {
