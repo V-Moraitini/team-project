74b1511f43c479878b7ef1eb9455feed0f7456f8
mnmz1 <mahamed.mahamud.2@city.ac.uk>
2023-04-08 09:36:50 +0100

    Access Blank Stock fully functional

--- src/GUI/GUISAAccessBlankStock.java
+++ src/GUI/GUISAAccessBlankStock.java
@@ -5,23 +5,38 @@ import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.DateFormat;
+import java.text.FieldPosition;
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
-public class GUISAAccessBlankStock extends JDialog{
+public class GUISAAccessBlankStock extends JDialog {
     private JPanel panel1;
     private JButton logOutButton;
     private JButton backButton;
     private JTable table1;
     private JButton addBlankButton;
     private JButton archiveBlankButton;
-
+    private JTextField blankIDtf;
+    private JTextField qtytf;
+    private JTextField blankTypetf;
+    private JTextField datetf;
+    private DefaultTableModel model;
+    DateFormat dateFormat = new SimpleDateFormat("dd/MM/YY");
+    Date date = new Date();
+    Calendar cal = Calendar.getInstance();
 
     public GUISAAccessBlankStock(JFrame parent) {
 
         super(parent);
         setTitle("Access Blank Stock");
+        model = (DefaultTableModel) table1.getModel();
+        datetf.setText(" " + dateFormat.format(date));
         createTable();
         setContentPane(panel1);
-        setMinimumSize(new Dimension(450, 274));
+        setMinimumSize(new Dimension(450, 700));
         setModal(true);
         setLocationRelativeTo(parent);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
@@ -45,19 +60,29 @@ public class GUISAAccessBlankStock extends JDialog{
         addBlankButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                dispose();
-                new GUISAAddNewBlank(null).setVisible(false);
-                panel1.setVisible(false);
-            }
+                if (blankIDtf.getText().equals("") ||qtytf.getText().equals("") || blankTypetf.getText().equals("")) {
+                    error();
+                }else{
+                    DefaultTableModel model = (DefaultTableModel) table1.getModel();
+                    model.addRow(new Object[]{blankIDtf.getText(),qtytf.getText(),blankTypetf.getText(),datetf.getText()});
+                } }
         });
 
         archiveBlankButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                dispose();
-                new GUISAArchiveBlank(null).setVisible(false);
-                panel1.setVisible(false);
-            }
+                DefaultTableModel model = (DefaultTableModel) table1.getModel();
+                if(table1.getSelectedRow()!=-1) {
+                //if a row is selected in the table
+                model.removeRow(table1.getSelectedRow());
+                }else if(table1.getRowCount()==0){
+                        JOptionPane.showMessageDialog(GUISAAccessBlankStock.this, "Select row");
+                    // If the table is empty with 0 rows
+                }else{
+                    JOptionPane.showMessageDialog(GUISAAccessBlankStock.this, "Please select a row");
+                }
+                }
+
         });
 
         setVisible(true);
@@ -69,8 +94,17 @@ public class GUISAAccessBlankStock extends JDialog{
     private void createTable(){
         table1.setModel(new DefaultTableModel(
                 null,
-                new String [] {"Blank ID", "Quantity", "Blank Type"}
-        ));}
+                new String [] {"Blank ID", "Quantity", "Blank Type", "Date"}
+
+
+        ));
+        DefaultTableModel model = (DefaultTableModel) table1.getModel();
+    }
+
+
+    private void error(){
+        JOptionPane.showMessageDialog(this, "Please enter all data");
+    }
 
     public static void main(String[] args) {
         GUISAAccessBlankStock panel = new GUISAAccessBlankStock(null);
